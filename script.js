d76b49fab5efff0cb54e778f0daf7feebfe5fdf8
ThZiNN77 <ferreira.luiz30@escola.pr.gov.br>
2026-08-14 09:24:31 -0300

    js

---
+++ script.js
@@ -0,0 +1,13 @@
+const caixaPrincipal = document.quarySelector(".caixa-primcipal")
+const caixaPerguntas = document.quarySelector(".caixa-perguntas")
+const caixaAlternativas = document.quarySelector(".caixa-alternativas")
+const caixaResultado = document.quarySelector(".caixa-resultado")
+const textoResultado = document.quarySelector(".texto-resultado")
+
+const perguntas = [
+    
+    "Perguntas 1" , 
+    
+    "Perguntas 2"
+
+]
